26f96f33548961cc2688afac0622c41368a2a0c8
CalCarmichael <callunclive@hotmail.com>
2017-04-03 19:23:13 +0100

    App Navigation
    Implemented navigation controller. Map and Profile Page now in navigation. Basic set up complete. Move on to recoding the pages

--- Skate Demo/AppDelegate.swift
+++ Skate Demo/AppDelegate.swift
@@ -19,7 +19,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
+        
+        //Change colour of tab bar items
+        
+        UITabBar.appearance().tintColor = .black
+        
         return true
     }
 
